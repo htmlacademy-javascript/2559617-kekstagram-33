ddae46b2cac32365236790a598787ad2c5b4cc00
Maksim <mmissuragin@gmail.com>
2024-11-12 18:21:39 +0300

    модуль 2 задание 1

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,17 @@
-// function isCheckingLenght(string, maxLength) {
-//   return string.length <= maxLength;
-// }
-// const result = isCheckingLenght('ТУТ13СИМВОЛОВ', 10);
-// console.log(result);
-
+function isCheckingLenght(string, maxLength) {
+  return string.length <= maxLength;
+}
+isCheckingLenght('проверяемая строка', 20);
+isCheckingLenght('проверяемая строка', 18);
+isCheckingLenght('проверяемая строка', 10);
 function isPalindrom(rawString) {
   const optimizeString = rawString.replaceAll(' ', '').toLowerCase();
   let reversString = '';
-
   for (let i = optimizeString.length - 1; i >= 0; i--) {
     reversString += optimizeString[i];
   }
   return optimizeString === reversString;
 }
-
-const result = isPalindrom('ДЕД');
-console.log(result);
+isPalindrom('топот');
+isPalindrom('ДоВод');
+isPalindrom('КеКс');
