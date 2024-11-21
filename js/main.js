743d733ea841e60c27486f56ba61173f35bda2f1
Maksim <mmissuragin@gmail.com>
2024-11-21 22:19:51 +0300

    правки по дз из 4 модуля

--- js/main.js
+++ js/main.js
@@ -20,7 +20,17 @@ const COMMENTS = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-const maxNumberComment = 30;
+const LIKE_COUNT_RANGE = {
+  min: 15,
+  max: 200
+};
+
+const IMAGE_ID_RANGE = {
+  min: 1,
+  max: 6
+};
+
+const maxQuantityComment = 30;
 const NUMBERS_OF_ARRAYS = 25;
 
 function getRandomIntInclusive(min, max) {
@@ -31,28 +41,38 @@ function getRandomIntInclusive(min, max) {
 
 const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
 
-function getCreateComment(id) {
+const createIdGenerator = () => {
+  let currentId = 0;
+  return () => ++currentId;
+};
+
+const getNextCommentId = createIdGenerator();
+const getNextPhotoId = createIdGenerator();
+const getNextUrlId = createIdGenerator();
+
+
+function getCreateComment() {
   return {
-    id: id,
-    avatar: `img/avatar-${getRandomIntInclusive(1, 6)}.svg`,
+    id: getNextCommentId(),
+    avatar: `img/avatar-${getRandomIntInclusive(IMAGE_ID_RANGE.min, IMAGE_ID_RANGE.max)}.svg`,
     message: `${getRandomArrayElement(COMMENTATOR_NAMES)}`,
     name: `${getRandomArrayElement(COMMENTS)}`,
   };
 }
 
-function getCreatePhotoInfo(id) {
-  const commentCount = getRandomIntInclusive(0, maxNumberComment);
-  const comments = Array.from({ length: commentCount }, (_, i) => getCreateComment(i + 1));
+function getCreatePhotoInfo() {
+  const commentCount = getRandomIntInclusive(0, maxQuantityComment);
+  const comments = Array.from({ length: commentCount }, () => getCreateComment());
   return {
-    id: id,
-    url: `photos/${getRandomIntInclusive(1, 6)}.jpg`,
+    id: getNextPhotoId(),
+    url: `photos/${getNextUrlId()}.jpg`,
     description: 'Это придуманное описание для фотографии',
-    likes: getRandomIntInclusive(15, 200),
+    likes: getRandomIntInclusive(LIKE_COUNT_RANGE.min, LIKE_COUNT_RANGE.max),
     comments: comments,
   };
 }
 
-const totalPhotosInfo = Array.from({ length: getRandomIntInclusive(1, NUMBERS_OF_ARRAYS) }, (_, index) => getCreatePhotoInfo(index + 1));
+const totalPhotosInfo = Array.from({ length: getRandomIntInclusive(1, NUMBERS_OF_ARRAYS) }, () => getCreatePhotoInfo());
 
 // eslint-disable-next-line no-console
 console.log(totalPhotosInfo);
