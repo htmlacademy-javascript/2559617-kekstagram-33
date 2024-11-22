4663cdd2b14406af5e5fc79f8913073631467b78
Maksim <mmissuragin@gmail.com>
2024-11-23 00:53:48 +0300

    дз модуль 5 задание 1

---
+++ js/util.js
@@ -0,0 +1,12 @@
+export function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
+
+export const createIdGenerator = () => {
+  let currentId = 0;
+  return () => ++currentId;
+};
