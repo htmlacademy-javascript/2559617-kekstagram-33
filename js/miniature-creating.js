e1212711eeeb49557e07a51e6f97679a273aa623
Maksim <mmissuragin@gmail.com>
2024-11-28 14:14:05 +0300

    попытка сделать дз 8

--- js/miniature-creating.js
+++ js/miniature-creating.js
@@ -1,6 +1,6 @@
 import { generatePhotosInfo } from './photo-info-generation';
 
-const photoTemplateElement = document.querySelector('#picture').content.querySelector('.picture'); 
+const photoTemplateElement = document.querySelector('#picture').content.querySelector('.picture');
 const photosContainer = document.querySelector('.pictures');
 
 const photosData = generatePhotosInfo();
@@ -19,3 +19,15 @@ photosData.forEach((photoData) => {
 photosContainer.appendChild(photosFragment);
 
 export { photosContainer };
+
+const bigPicture = document.querySelector('.big-picture');
+const bigPictureImg = bigPicture.querySelector('.big-picture__img');
+const likesCount = bigPicture.querySelector('.likes-count');
+const shownCommentsCount = bigPicture.querySelector('.social__comment-shown-count');
+const totalCommentsCount = bigPicture.querySelector('.social__comment-total-count');
+const commentsList = bigPicture.querySelector('.social__comments');
+const caption = bigPicture.querySelector('.social__caption');
+const commentCounter = bigPicture.querySelector('.social__comment-count');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
+const cancelButton = bigPicture.querySelector('.big-picture__cancel');
+
