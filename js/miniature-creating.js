a926e2237f327da58d307ee755fbab17453b3142
Maksim <mmissuragin@gmail.com>
2024-11-28 18:26:44 +0300

    вернулся в начальное состояние

--- js/miniature-creating.js
+++ js/miniature-creating.js
@@ -19,15 +19,3 @@ photosData.forEach((photoData) => {
 photosContainer.appendChild(photosFragment);
 
 export { photosContainer };
-
-const bigPicture = document.querySelector('.big-picture');
-const bigPictureImg = bigPicture.querySelector('.big-picture__img');
-const likesCount = bigPicture.querySelector('.likes-count');
-const shownCommentsCount = bigPicture.querySelector('.social__comment-shown-count');
-const totalCommentsCount = bigPicture.querySelector('.social__comment-total-count');
-const commentsList = bigPicture.querySelector('.social__comments');
-const caption = bigPicture.querySelector('.social__caption');
-const commentCounter = bigPicture.querySelector('.social__comment-count');
-const commentsLoader = bigPicture.querySelector('.comments-loader');
-const cancelButton = bigPicture.querySelector('.big-picture__cancel');
-
