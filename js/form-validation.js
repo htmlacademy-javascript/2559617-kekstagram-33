bb8559bc7e98b31d56811485a944d9a129c35436
Maksim <mmissuragin@gmail.com>
2024-12-03 00:40:01 +0300

    выполнение ДЗ из 9 модуля

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,95 +1,89 @@
 import '../vendor/pristine/pristine.min.js';
 
-// import { isEscapeKey } from './util';
-
 const dataCommentField = Object.freeze({
   MAX_LENGTH: 140,
-  MESSAGE_ERROR: 'Длина комментария не должна превышать 140 символов'
+  MESSAGE_ERROR: 'Длина комментария не должна превышать 140 символов',
 });
 
 const dataHashtagField = Object.freeze({
   MAX_HASHTAG_NUMBERS: 5,
-  HASHTAG_NOT_VALID: 'Хэштег доолжен начинаться с #',
+  HASHTAG_NOT_VALID: 'Хэштег должен начинаться с #',
+  DUPLICATE_HASHTAGS: 'Хэштеги не должны повторяться',
   MAX_HASHTAG_NUMBERS_EXCEEDED: 'Количество хэштегов должно быть не больше 5',
-  DUPLICATE_HASHTAGS: 'Хэштеги не должны повторяться'
 });
-
-const formUploadDOMElement = document.querySelector('.img-upload__form');
-const hashtagsInputDOMElement = formUploadDOMElement.querySelector('.text__hashtags');
-const commentFieldDOMEElement = formUploadDOMElement.querySelector('.text__description');
-
-const pristine = new Pristine(formUploadDOMElement, {
-  classTo: 'img-upload__field-wrapper',
-  errorTextParent: 'img-upload__field-wrapper',
-  errorTextClass: 'img-upload__field-wrapper--error',
-}, false);
 
 const HASHTAG_REGEX = /^#[a-zа-яё0-9]{1,19}$/i;
 const isValidHashtag = (hashtag) => HASHTAG_REGEX.test(hashtag);
 const uniqValueHashtag = (array) => array.length === new Set(array).size;
 
-
 const checkOnValidHashtag = (hashtags) => {
   if (!hashtags.every(isValidHashtag)) {
     return dataHashtagField.HASHTAG_NOT_VALID;
   }
+  return null;
 };
 
 const checkOnNumbersHashtags = (hashtags) => {
   if (hashtags.length > dataHashtagField.MAX_HASHTAG_NUMBERS) {
     return dataHashtagField.MAX_HASHTAG_NUMBERS_EXCEEDED;
   }
+  return null;
 };
 
 const checkOnDuplicateHashtag = (hashtags) => {
   if (!uniqValueHashtag(hashtags)) {
     return dataHashtagField.DUPLICATE_HASHTAGS;
   }
+  return null;
 };
 
 const rulesValidationHashtags = [
   checkOnValidHashtag,
   checkOnNumbersHashtags,
-  checkOnDuplicateHashtag
+  checkOnDuplicateHashtag,
 ];
 
 let hashtagErrorMessage = [];
 
 const validateHashtagField = (value) => {
-  const hashtags = value.split(/\s/).map((hashtag) => hashtag.toLowerCase()).filter(Boolean);
+  const hashtags = value.trim().split(/\s+/).map((hashtag) => hashtag.toLowerCase()).filter(Boolean);
   hashtagErrorMessage = [];
-
-  rulesValidationHashtags.reduce((errors, validator) => {
-    const error = validator(hashtags);
-
-    if(error) {
-      errors.push(error);
-    }
-
-    return errors;
-  }, hashtagErrorMessage);
-
+  hashtagErrorMessage = rulesValidationHashtags
+    .map((validator) => validator(hashtags))
+    .filter(Boolean);
   return !hashtagErrorMessage.length;
 };
 
 const validateCommentField = (value) => value.length <= dataCommentField.MAX_LENGTH;
 
 const getHashtagErrorMessage = () => hashtagErrorMessage[0] ?? '';
+const getCommentErrorMessage = () => dataCommentField.MESSAGE_ERROR;
 
-const getCommentErroeMessage = () => dataCommentField.MESSAGE_ERROR;
+let formElement;
+let hashtagsInput;
+let commentField;
 
-pristine.addValidator(hashtagsInputDOMElement, validateHashtagField, getHashtagErrorMessage);
-pristine.addValidator(commentFieldDOMEElement, validateCommentField, getCommentErroeMessage);
 
-const checkValidateForm = () => {
-  formUploadDOMElement.addEventListener('submit', (evt) => {
+export const initValidation = (form, hashtags, comment) => {
+  formElement = form;
+  hashtagsInput = hashtags;
+  commentField = comment;
+  const pristine = new Pristine(form, {
+    classTo: 'img-upload__field-wrapper',
+    errorTextParent: 'img-upload__field-wrapper',
+    errorTextClass: 'img-upload__field-wrapper--error',
+  }, false);
+  pristine.addValidator(hashtagsInput, validateHashtagField, getHashtagErrorMessage);
+  pristine.addValidator(commentField, validateCommentField, getCommentErrorMessage);
+  return pristine;
+};
+
+export const checkValidateForm = (pristine) => {
+  formElement.addEventListener('submit', (evt) => {
     evt.preventDefault();
-
     const isValid = pristine.validate();
     if (isValid) {
-      formUploadDOMElement.submit();
+      formElement.submit();
     }
   });
 };
-
-export {checkValidateForm};
--- js/form.js
+++ js/form.js
@@ -1,16 +1,48 @@
 import { updateScale } from './scale.js';
 import { initEffects } from './effects.js';
-import { checkValidateForm } from './form-validation.js';
-// import { isEscapeKey } from './util';
+import { initValidation, checkValidateForm } from './form-validation.js';
+import { isEscapeKey } from './util.js';
 
 const fileInput = document.getElementById('upload-file');
 const overlay = document.querySelector('.img-upload__overlay');
 const bodyElement = document.querySelector('body');
 const cancelButton = document.getElementById('upload-cancel');
+const formUploadDOMElement = document.querySelector('.img-upload__form');
+const hashtagsInputDOMElement = formUploadDOMElement.querySelector('.text__hashtags');
+const commentFieldDOMElement = formUploadDOMElement.querySelector('.text__description');
+
+
+let pristine;
 
 const closeOverlay = () => {
   overlay.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
+  // eslint-disable-next-line no-use-before-define
+  document.removeEventListener('keydown', onDocumentEscPress);
+  if (pristine) {
+    pristine.reset();
+  }
+  formUploadDOMElement.reset();
+};
+
+hashtagsInputDOMElement.addEventListener('keydown', (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.stopPropagation();
+  }
+});
+
+commentFieldDOMElement.addEventListener('keydown', (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.stopPropagation();
+  }
+});
+
+
+const onDocumentEscPress = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeOverlay();
+  }
 };
 
 fileInput.addEventListener('change', () => {
@@ -18,7 +50,9 @@ fileInput.addEventListener('change', () => {
   bodyElement.classList.add('modal-open');
   updateScale(100);
   initEffects();
-  checkValidateForm();
+  pristine = initValidation(formUploadDOMElement, hashtagsInputDOMElement, commentFieldDOMElement);
+  checkValidateForm(pristine);
+  document.addEventListener('keydown', onDocumentEscPress);
 });
 
 cancelButton.addEventListener('click', closeOverlay);
