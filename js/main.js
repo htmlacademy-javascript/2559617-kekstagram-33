4e59c94469597fcb20fd009346beb54ed0d4ca83
Maksim <mmissuragin@gmail.com>
2024-12-02 22:36:35 +0300

    обработка ошибки линтера

--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
-// import './thumbnails.js';
-// import './full-image-view.js';
-
-import {photosContainer} from './miniature-creating';
-
-//eslint-disable-next-line no-console
-console.log(photosContainer);
+import './miniature-creating';
