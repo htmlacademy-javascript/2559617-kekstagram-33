e47c65e1aa0efddca4f323bd5c5e25cb8692590c
Maksim <mmissuragin@gmail.com>
2024-12-06 16:23:22 +0300

    выполнение ДЗ 12.1

---
+++ js/gallery-filtering.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-shadow */
+import { renderPhotos } from './miniature-creating.js';
+import { debounce, sortArray, compareComments } from './util.js';
+
+const imageFiltersButtons = document.querySelectorAll('.img-filters__button');
+const pictures = document.querySelector('.pictures');
+
+function onFiltersChange(filter) {
+  imageFiltersButtons.forEach((button) => {
+    button.classList.remove('img-filters__button--active');
+  });
+  filter.classList.add('img-filters__button--active');
+  return filter.id;
+}
+
+const removeMiniatures = () => {
+  const addedPictures = pictures.querySelectorAll('.picture');
+  addedPictures.forEach((picture) => {
+    pictures.removeChild(picture);
+  });
+};
+
+export const sortPictures = (initialPictures, cb) => {
+  imageFiltersButtons.forEach((currentButton) => {
+    const sortPicturesDebounce = debounce((filter) => {
+      removeMiniatures();
+      let currentPictures = [...initialPictures];
+
+      switch (filter) {
+        case 'filter-random':
+          currentPictures = sortArray(currentPictures)
+            .slice(0, 10);
+          break;
+        case 'filter-discussed':
+          currentPictures = initialPictures.slice()
+            .sort(compareComments);
+          break;
+        default:
+          currentPictures = initialPictures;
+          break;
+      }
+      renderPhotos(currentPictures, cb);
+    }, 500);
+
+    currentButton.addEventListener('click', () => {
+      const filterType = onFiltersChange(currentButton);
+      sortPicturesDebounce(filterType);
+    });
+  });
+};
--- js/main.js
+++ js/main.js
@@ -3,13 +3,15 @@
 import '../vendor/pristine/pristine.min.js';
 import './photo-info-generation.js';
 import './form.js';
+import {sortPictures} from './gallery-filtering.js';
 import {getData} from './api.js';
-import { renderPhotos } from './miniature-creating.js';
-import { getDataError} from './post-messages.js';
+import {renderPhotos} from './miniature-creating.js';
+import {getDataError} from './post-messages.js';
 
 getData()
   .then((picture) => {
     renderPhotos(picture);
+    sortPictures(picture);
   })
   .catch(
     (_err) => {
